eacf19e6a56e39bcfd285fe5ce30e52056fb5b1e
Dougly <dgalante11@gmail.com>
2016-08-19 20:57:25 -0400

    playing with print function in swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favColor = "Purple"
+var favWord = "Lake"
+var favMusician = "Kurt Vile"
+
+print (favColor)
+print (favWord)
+print (favMusician)
+print ("Im printing a string in swift")
+print ("hi frineds my favorite color is purple")
+var color = "blue"
+print ("the sky is \(color)")
+var sentence = "may favorite musician is \(favMusician)"
+print(sentence)
