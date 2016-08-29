a88d2da9ddd9d2fae1e6e58062dc149bbb0800c4
RWoolard <r.woolard456@gmail.com>
2016-08-29 15:35:57 -0400

    done

---
+++ RW Print Lab.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var favoriteWord = "money"
+var favoriteColor = "red"
+var favoriteMusician = "Chick Corea"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is money, my favorite color is red, and my favorite musician is Chick Corea.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
